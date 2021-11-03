e4044b0d53cd3272aa4293461969ee9d69f5a9cb
tuyen061103 <93665094+tuyen061103@users.noreply.github.com>
2021-11-03 22:10:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Hello BâyyBêê",
-    introTitle: 'NGỌC HIỀN',
+    introTitle: 'Gửi đến ai đó',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,7 +9,7 @@ const CONFIG = {
     title: 'Yêu hay không yêu không yêu hay yêu nói một lời 🥰',
     desc: 'Nói thật đê yêu hay không yêu? ',
     btnYes: 'Yêu lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Đéo yêu :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Vì anh đẹp zai hiền lành vui tính <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
